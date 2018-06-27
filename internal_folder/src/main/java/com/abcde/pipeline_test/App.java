26f5e3e946015adf43421474cb173d24a53da170
Vatsalgarg1617 <vatsalgarg17@gmail.com>
2018-06-27 11:13:58 +0530

    First Commit

---
+++ internal_folder/src/main/java/com/abcde/pipeline_test/App.java
@@ -0,0 +1,13 @@
+package com.abcde.pipeline_test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
